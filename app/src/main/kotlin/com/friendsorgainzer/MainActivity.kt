528e0010035dc3d708ad31048d472277c5c40d98
mvomelchenko <MVOMELCHEN@mts.ru>
2023-10-21 10:52:44 +0300

    add theme changer | id on list and position number

--- app/src/main/kotlin/com/friendsorgainzer/MainActivity.kt
+++ app/src/main/kotlin/com/friendsorgainzer/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.friendsorgainzer.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -32,17 +30,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        val appBarConfiguration =
-            AppBarConfiguration(
-                setOf(
-                    R.id.navigation_home,
-                    R.id.navigation_dashboard,
-                    R.id.navigation_notifications
-                )
-            )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
